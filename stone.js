ded7f59942622012d3399fb4a8dd00ee0e468948
thecodingking10 <67853976+thecodingking10@users.noreply.github.com>
2021-09-21 16:36:55 +0530

    done

---
+++ stone.js
@@ -0,0 +1,31 @@
+class Stone{
+	constructor(x,y)
+	{
+		var options = {
+			restitution: 0.8, 
+			friction: 3,
+			density: 30
+		}
+	// assign options to the rubber ball
+		this.x=x;
+		this.y=y;
+		this.width = 80;
+        this.height = 50
+		this.body=Bodies.rectangle(this.x, this.y,this.width, this.height, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black"); 
+			fill("black");
+			//draw the ellipse here to display the rubber ball
+			rect(0,0,this.width,this.height)
+
+			pop()
+	}}
